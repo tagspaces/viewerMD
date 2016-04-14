e9756dce4eb3023ded262f1391dfa0f9d1ce2dd4
uggrock <ugg.rock@gmail.com>
2016-04-14 17:39:18 +0200

    small inprovements

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ var isCordova;
 var isWin;
 var isWeb;
 
+var $htmlContent;
+
 $(document).ready(function() {
   function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
@@ -24,11 +26,11 @@ $(document).ready(function() {
   isCordova = parent.isCordova;
   isWin = parent.isWin;
   isWeb = parent.isWeb;
-  
+
   $(document).on('drop dragend dragenter dragover', function(event) {
     event.preventDefault();
   });
-  
+
   $('#aboutExtensionModal').on('show.bs.modal', function() {
     $.ajax({
       url: 'README.md',
@@ -42,12 +44,12 @@ $(document).ready(function() {
         handleLinks(modalBody);
       } else {
         console.log("markdown to html transformer not found");
-      }        
+      }
     })
     .fail(function(data) {
       console.warn("Loading file failed " + data);
     });
-  });  
+  });
 
   function handleLinks($element) {
     $element.find("a[href]").each(function() {
@@ -60,7 +62,7 @@ $(document).ready(function() {
     });
   }
 
-  var $htmlContent = $("#htmlContent");
+  $htmlContent = $("#htmlContent");
 
   var styles = ['', 'solarized-dark', 'github', 'metro-vibes', 'clearness', 'clearness-dark'];
   var currentStyleIndex = 0;
@@ -82,6 +84,13 @@ $(document).ready(function() {
     if (currentStyleIndex >= styles.length) {
       currentStyleIndex = 0;
     }
+    $htmlContent.removeClass();
+    $htmlContent.addClass('markdown ' + styles[currentStyleIndex] + " " + zoomSteps[currentZoomState]);
+    saveExtSettings();
+  });
+
+  $("#resetStyleButton").bind('click', function() {
+    currentStyleIndex = 0;
     $htmlContent.removeClass();
     $htmlContent.addClass('markdown ' + styles[currentStyleIndex] + " " + zoomSteps[currentZoomState]);
     saveExtSettings();
@@ -148,10 +157,10 @@ $(document).ready(function() {
 });
 
 function setContent(content, fileDirectory) {
-  var $htmlContent = $('#htmlContent');
+  $htmlContent = $("#htmlContent");
   $htmlContent.append(content);
 
-  $("base").attr("href", fileDirectory + "//");
+  //$("base").attr("href", fileDirectory + "//");
 
   if (fileDirectory.indexOf("file://") === 0) {
     fileDirectory = fileDirectory.substring(("file://").length, fileDirectory.length);
